4ac470784740e364efcfcc2ed622253b2d2ab5f5
joaoeudes2501 <joaoeudes2501200@gmail.com>
2022-12-27 12:59:38 +0000

    first commit

---
+++ secret/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro:["Motor" , "Porta" , "Capô" , "Pneu" , "Antena"],
+    Fruta:["Banana" , "Maçã" , "Pêra" , "Mamão" , "Laranja"],
+    Corpo:["Braço" , "Perna" , "Cérebro" , "Pescoço" , "Olhos"],
+    Computador:["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação:["Linguagem", "Framework", "JavaScript", "React"],
+    alimento:["Arroz", "Feijão", "Carne", "Leite",  "Ovo"]
+}
