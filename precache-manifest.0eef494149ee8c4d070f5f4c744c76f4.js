a1e4606bca9be912c1ace0ce659ff3651117d200
joaoeudes2501 <joaoeudes2501200@gmail.com>
2022-12-27 13:13:31 +0000

    Updates

---
+++ precache-manifest.0eef494149ee8c4d070f5f4c744c76f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "820db767cce00fe5b6c0",
+    "url": "/Secret-word/static/css/main.18067cb6.chunk.css"
+  },
+  {
+    "revision": "820db767cce00fe5b6c0",
+    "url": "/Secret-word/static/js/main.820db767.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/Secret-word/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "eaa66edd87c40f5ea1d1",
+    "url": "/Secret-word/static/js/2.eaa66edd.chunk.js"
+  },
+  {
+    "revision": "416dea2d67befef99343",
+    "url": "/Secret-word/static/js/runtime~main.416dea2d.js"
+  },
+  {
+    "revision": "8330a7832b7dbc18c94394badd39decc",
+    "url": "/Secret-word/index.html"
+  }
+];
